891ede1b7bf2957a2fec4e1684de1788d950847a
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2023-12-08 10:36:33 +0800

    Speed up testing
    By disabling address sanitizer when testing the accuracy
    
    Also adds codes for measuring latency for each command

--- utils/run-test.py
+++ utils/run-test.py
@@ -2,8 +2,19 @@ import os
 import pathlib
 from subprocess import (check_call, CalledProcessError)
 import sys
+import time
 
 TOPDIR = pathlib.Path(__file__).absolute().parents[1]
+
+def check_call_verbose(*popenargs, **kwargs):
+    try:
+        args = popenargs[0]
+    except IndexError:
+        args = kwargs['args']
+
+    print(f'Starting command {args} at timestamp {time.time()}')
+
+    return check_call(*popenargs, **kwargs)
 
 def build_and_test(config, suffix, intermittent):
     try:
@@ -21,10 +32,10 @@ def build_and_test(config, suffix, intermittent):
         except ValueError:
             pass
         my_debug = 3
-    check_call([sys.executable, TOPDIR / 'dnn-models' / 'transform.py', *config])
+    check_call_verbose([sys.executable, TOPDIR / 'dnn-models' / 'transform.py', *config])
 
-    check_call(['cmake', '-S', TOPDIR, '-B', 'build', '-DBUILD_MSP432=OFF', f'-DMY_DEBUG={my_debug}'])
-    check_call(['make', '-C', 'build'])
+    check_call_verbose(['cmake', '-S', TOPDIR, '-B', 'build', '-DBUILD_MSP432=OFF', f'-DMY_DEBUG={my_debug}'])
+    check_call_verbose(['make', '-C', 'build'])
 
     rounds = 100
     power_cycle = 0.03
@@ -43,7 +54,7 @@ def build_and_test(config, suffix, intermittent):
             '--suffix', suffix,
             '--compress',
         ] + run_cmd
-    check_call(run_cmd, env={'TMPDIR': '/var/tmp'})
+    check_call_verbose(run_cmd, env={'TMPDIR': '/var/tmp'})
 
 def main():
     # preparation
