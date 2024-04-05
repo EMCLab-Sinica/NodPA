cfb367913fb759a31104b3619e187f6954c0fa41
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2024-04-05 17:35:50 +0800

    Put buildbot configurations here
    Extracted from my buildbot repo
    https://gitlab.com/yan12125/buildbot/-/blob/5121c960f0a2cb37233e6f3864a81f2d0d1e7fbf/stateful_cnn.py#L104

---
+++ utils/generate-buildbot-config.py
@@ -0,0 +1,58 @@
+import dataclasses
+import itertools
+import json
+import pathlib
+from typing import Any
+
+@dataclasses.dataclass
+class ModelConfig:
+    model: str
+    targets: list[str]
+    approaches: list[str]
+    batch_sizes: list[int]
+
+def config_builder(model: str, batch_size: int, approach: str, target: str) -> dict[str, Any]:
+    config = f'--target {target} --{approach} --batch-size {batch_size} {model}'
+    if approach == 'ideal' or approach == 'stateful' and batch_size == 1:
+        config += ' --all-samples'
+
+    suffix = f'{model}_{approach}_b{batch_size}'
+
+    if target == 'msp432':
+        suffix += '_cmsis'
+
+    return {
+        'builder_name': f'stateful-cnn-{suffix}',
+        'command_env': {
+            'CONFIG': config,
+            'LOG_SUFFIX': suffix.replace('.', '_'),
+        },
+    }
+
+def main() -> None:
+    all_targets = ['msp430', 'msp432']
+    all_approaches = ['hawaii', 'japari', 'stateful']
+    all_batch_sizes = [1, 2]
+    ideal_config = {'targets': all_targets, 'approaches': ['ideal'], 'batch_sizes': [1]}
+    complete_config = {'targets': all_targets, 'approaches': all_approaches, 'batch_sizes': all_batch_sizes}
+    model_configs = [
+        ModelConfig(model='cifar10', **ideal_config),
+        ModelConfig(model='cifar10', **complete_config),
+        ModelConfig(model='kws', **ideal_config),
+        ModelConfig(model='kws', **complete_config),
+        ModelConfig(model='har', **ideal_config),
+        ModelConfig(model='har', targets=all_targets, approaches=all_approaches, batch_sizes=[1]),
+        ModelConfig(model='transformers', **ideal_config),
+    ]
+
+    buildbot_configurations = []
+    for m in model_configs:
+        for target, approach, batch_size in itertools.product(m.targets, m.approaches, m.batch_sizes):
+            buildbot_configurations.append(config_builder(m.model, batch_size, approach, target))
+
+    output_path = pathlib.Path(__file__).resolve().parent / 'buildbot-config.json'
+    with open(output_path, 'w') as f:
+        json.dump(buildbot_configurations, f, indent=4)
+
+if __name__ == '__main__':
+    main()
