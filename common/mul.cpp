34c41d1701a3c479b6a40a59b55e31294a6077fd
Chih-Hsuan Yen <645432-yan12125@users.noreply.gitlab.com>
2024-04-14 21:45:15 +0800

    mul: implement HAWAII and Stateful

--- common/mul.cpp
+++ common/mul.cpp
@@ -1,5 +1,8 @@
 #include <cstdint>
 #include "cnn_common.h"
+#include "counters.h"
+#include "data.h"
+#include "intermittent-cnn.h"
 #include "layer-defs.h"
 #include "my_debug.h"
 #include "my_dsplib.h"
@@ -20,6 +23,25 @@ void handle_mul(Model *model, const ParameterInfo *input[], ParameterInfo *outpu
 
     uint32_t data_offset = 0;
 
+#if INTERMITTENT
+    start_cpu_counter(offsetof(Counters, progress_seeking));
+    uint32_t first_unfinished_job_idx = run_recovery(model, output);
+    data_offset = batch_start(job_index_to_offset(output, first_unfinished_job_idx));
+
+#if INDIRECT_RECOVERY
+    start_cpu_counter(offsetof(Counters, state_query));
+    uint16_t next_output_turning_point;
+    int16_t offset;
+    uint8_t output_turning_point_idx;
+    SlotInfo *output_slot_info;
+    find_initial_state_bit(&offset, &output_turning_point_idx, &next_output_turning_point, &output_slot_info,
+                           data_offset, model, output);
+    stop_cpu_counter();
+#endif
+
+    stop_cpu_counter();
+#endif
+
     uint16_t buffer_size = 256;
     int16_t *buffer_x = lea_buffer,
             *buffer_y = buffer_x + buffer_size;
@@ -27,19 +49,52 @@ void handle_mul(Model *model, const ParameterInfo *input[], ParameterInfo *outpu
 
     int16_t scaleFract;
     uint8_t shift;
-    float_to_scale_params(&scaleFract, &shift, q15_to_float(*buffer_y, ValueInfo(Y)));
+    float_to_scale_params(&scaleFract, &shift, q15_to_float(*buffer_y, ValueInfo(Y), /*p_use_prefix=*/nullptr, /*has_state=*/false));
 
     for (; data_offset < output->params_len / sizeof(int16_t); data_offset += buffer_size) {
         uint16_t cur_buffer_size = MIN_VAL(output->params_len / sizeof(int16_t) - data_offset, buffer_size);
         my_printf_debug("data_offset=%d" NEWLINE, data_offset);
         my_memcpy_from_param(model, buffer_x, X, data_offset, cur_buffer_size * sizeof(int16_t));
 
+#if STATEFUL
+        my_printf_debug("Before strip states" NEWLINE);
+        dump_matrix_debug(buffer_x, cur_buffer_size, ValueInfo(output), false);
+
+        for (uint16_t val_idx = BATCH_SIZE - 1; val_idx < cur_buffer_size; val_idx += BATCH_SIZE) {
+            strip_state(buffer_x + val_idx);
+        }
+
+#endif
+        my_printf_debug("Before mul" NEWLINE);
+        dump_matrix_debug(buffer_x, cur_buffer_size, ValueInfo(output), false);
+
         my_scale_q15(buffer_x, scaleFract, shift, buffer_x, buffer_size);
         my_printf_debug("After mul" NEWLINE);
         dump_matrix_debug(buffer_x, cur_buffer_size, ValueInfo(output), false);
 
+#if INDIRECT_RECOVERY
+        start_cpu_counter(offsetof(Counters, state_query));
+        fill_state_offsets(data_offset, cur_buffer_size, &offset, &output_turning_point_idx, &next_output_turning_point, output_slot_info);
+        stop_cpu_counter();
+        start_cpu_counter(offsetof(Counters, embedding));
+        update_states(buffer_x, cur_buffer_size, true);
+        stop_cpu_counter();
+        my_printf_debug("After embedding states" NEWLINE);
+        dump_matrix_debug(buffer_x, cur_buffer_size, ValueInfo(output), true);
+#endif
+
         my_memcpy_to_param(output, data_offset, buffer_x, cur_buffer_size * sizeof(int16_t), 0, true);
+
+#if HAWAII
+        write_hawaii_layer_footprint(model->layer_idx, cur_buffer_size);
+#endif
     }
+
+#if INDIRECT_RECOVERY
+    start_cpu_counter(offsetof(Counters, table_updates));
+    flip_state_bit(model, output);
+    stop_cpu_counter();
+#endif
 
     dump_params_debug(model, output, node->output_name);
 }
